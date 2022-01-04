68a7db3056dc73231a37883c9f6d9df8afbaacb3
klever1988 <56048681+klever1988@users.noreply.github.com>
2022-01-05 04:20:04 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -eu
 set -x
 
 telem=${TELEMETRY-false}
-plugins=${PLUGINS-xuqingfeng/caddy-rate-limit,pyed/ipfilter,miekg/caddy-prometheus}
+plugins=${PLUGINS-xuqingfeng/caddy-rate-limit,pyed/ipfilter,hiphref/caddy-geoip}
 
 echo 'go get' && {
     go get -v github.com/caddyserver/caddy"${VERSION+@}${VERSION-}"
